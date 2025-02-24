600c3c1d8a3799f9b9f1c7b11ac004a4b0ed6597
Lutfor Rahman <lutfor.shimanto2041@gmail.com>
2025-02-24 18:02:48 +0600

    feat: add logging configuration and standardize string quotes in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,31 @@
-import createNextIntlPlugin from "next-intl/plugin";
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
   images: {
-    domains: ["placehold.jp"],
+    domains: ['placehold.jp'],
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "utfs.io",
+        protocol: 'https',
+        hostname: 'utfs.io',
       },
       {
-        protocol: "https",
-        hostname: "source.unsplash.com",
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
       },
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
       },
       {
-        hostname: "1t2k916rlb.ufs.sh",
+        hostname: '1t2k916rlb.ufs.sh',
       },
     ],
   },
